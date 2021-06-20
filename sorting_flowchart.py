8fe4b3a3dca063e8a8fff586a06aadc2979769b6
AsiaSikora <84456280+AsiaSikora@users.noreply.github.com>
2021-06-20 13:42:08 +0200

    implemented function bubble_sort

--- sorting_flowchart.py
+++ sorting_flowchart.py
@@ -1,1 +1,33 @@
-# write your code here
+def bubble_sort():
+    array = [1, 2, 56, 32, 51, 2, 8, 92, 15]
+
+    print(array)
+
+    n = len(array)
+    i = 0
+    j = 0
+
+    while True:
+        if array[j] > array[j + 1]:
+            array[j], array[j + 1] = array[j + 1], array[j]
+        j += 1
+
+        if j < n - i - 1:
+            continue
+        else:
+            j = 0
+            i += 1
+            if j < n - i - 1:
+                continue
+            else:
+                break
+
+    print(array)
+
+
+def main():
+    bubble_sort()
+
+
+if __name__ == "__main__":
+    main()
